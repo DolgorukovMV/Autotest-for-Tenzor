6eb1ce92b5737848dfef1816860e1e4fe7bd863d
DolgorukovMV <142866232+DolgorukovMV@users.noreply.github.com>
2023-12-10 15:40:45 +0300

    Update contacts_page.py

--- autotest/The second scenario/pages/contacts_page.py
+++ autotest/The second scenario/pages/contacts_page.py
@@ -6,13 +6,17 @@ import logging
 
 # Настройка логгера
 logger = logging.getLogger(__name__)
-
+# Класс ContactsPage, наследующийся от BasePage
 class ContactsPage(BasePage):
+    # Метод для перехода на страницу "Контакты"
     def go_to_contacts(self):
         try:
+            # Открываем указанный URL
             self.open_url(self.base_url)
+            # Находим ссылку "Контакты" и кликаем
             contacts_link = self.wait_for_element(By.LINK_TEXT, "Контакты")
             contacts_link.click()
         except Exception as ex:
+            # Логируем ошибку и повторно поднимаем исключение
             logger.error(f"Exception in go_to_contacts: {ex}")
             raise  # Повторно поднимаем исключение
